e5e477cb9aab273f82eaa4ac3a271d1573268649
Ethan Muldoon <muldoone@oregonstate.edu>
2025-11-13 17:01:47 -0600

    Added some printouts to show that the tar is actually working.

--- src/runners/merge_tar.py
+++ src/runners/merge_tar.py
@@ -8,7 +8,9 @@ def merge_tar(output: str, inputs: list[str]) -> None:
     """Merge the input files into a tar.gz archive"""
     with tarfile.open(output,"w:gz") as tar:
         for file in inputs:
-            tar.add(file, os.path.basename(file))
+            name = os.path.basename(file)
+            print(f"Adding {name}")
+            tar.add(file, name)
 
 if __name__ == "__main__":
     output_file = sys.argv[1]
