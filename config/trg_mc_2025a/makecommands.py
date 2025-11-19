9b1b8d001d3c09986693803cba2e8ce81a7a5016
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-11-19 11:04:01 -0800

    try to fix output NAME

--- config/trg_mc_2025a/makecommands.py
+++ config/trg_mc_2025a/makecommands.py
@@ -1,10 +1,12 @@
 import os,sys,csv
 
 import csv
+from datetime import datetime, timezone
 
+timestamp: str = datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%S")
 
 tasklist = os.path.join(os.getenv("MERGE_UTILS_DIR"),"config","trg_mc_2025a",'trg_mc_2025a_jobs.csv')
-
+maxjob = 2000
 tasks = {}
 with open(tasklist,encoding='utf-8-sig') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -23,16 +25,17 @@ if task not in tasks:
 config = tasks[task]["FCL"].replace('.fcl','.json')
 nfiles = int(tasks[task]['NFILES'])
 f = open(f'{task}.sh','w')
-if nfiles < 7500:    
-    command = f"merge -vv -c trg_mc_2025a/{config} --tag=\"{task}\" query \" files from {tasks[task]['QUERY']}\""
+print ("nfiles",nfiles)
+if nfiles < maxjob:    
+    command = f"merge -vv -c trg_mc_2025a/{config} --tag=\"{task}\" query \" files from {tasks[task]['QUERY']}\" > {task}_{timestamp}_{skip}.log 2>&1 "
     print(command)
     f.write(command + '\n')
 else:
-    step = 5000
+    step = maxjob
     skip = 0
     while skip < nfiles:
-        command = f"merge -vv -c trg_mc_2025a/{config} --skip {skip} --limit {step} --tag=\"{task}\" query \" files from {tasks[task]['QUERY']}\""
-        print (command)
+        command = f"merge -vv -c trg_mc_2025a/{config} --skip={skip} --limit={step}  --tag=\"{task}\" query \" files from {tasks[task]['QUERY']}\" > {task}_{timestamp}_{skip}.log 2>&1 "
+        print(command)
         f.write(command + '\n')
         skip += step
 f.close()
