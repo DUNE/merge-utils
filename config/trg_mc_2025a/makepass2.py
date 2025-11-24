22999e8fa34bd2d9bf2d710b692452bc793e62e9
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-11-24 11:06:15 -0800

    add in pass2 json for production

--- config/trg_mc_2025a/makepass2.py
+++ config/trg_mc_2025a/makepass2.py
@@ -36,5 +36,7 @@ if task not in tasks:
         tasks[task]["TAG"]=task
 
 query = "files where merge.tag=%s and dune.output_status=confirmed"%tag
+check = "metacat query -s %s"%query
+print (check)
 command = "merge -c trg_mc_2025a/hadd_prod.json --tag=%s-pass2 query \"%s\""%(tag,query)
-print ("command",command)
+print ("if ok \n",command)
