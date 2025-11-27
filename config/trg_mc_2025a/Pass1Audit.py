0aacc3a51e6db15254e3216791b36b05792968c8
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-11-26 19:05:21 -0800

    change large to only do one pass

---
+++ config/trg_mc_2025a/Pass1Audit.py
@@ -0,0 +1,71 @@
+import os, sys
+from metacat.webapi import MetaCatClient
+
+import csv
+
+mc_client = MetaCatClient(os.environ["METACAT_SERVER_URL"])
+
+tasklist = os.path.join(os.getenv("MERGE_UTILS_DIR"),"config","trg_mc_2025a",'trg_mc_2025a_jobs.csv')
+maxjob = 2000
+tasks = {}
+
+with open(tasklist,encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+    #print(reader.fieldnames)
+    
+    for row in reader:
+        #print(row["TAG"], row['FCL'], row['QUERY'])
+        tasks[row["TAG"]] = row 
+    
+if len(sys.argv)<2:
+    task="HELP"
+else:
+    task = sys.argv[1]
+if task not in tasks:
+    
+        print(f"Task {task} not found")
+        print("Available tasks:", ', '.join(tasks.keys()))
+        sys.exit(1)
+nfiles = int(tasks[task]['NFILES'])
+#f = open(f'{task}.sh','w')
+print ("nfiles",nfiles)
+
+query = "files where merge.tag=%s and dune.output_status=confirmed"%sys.argv[1]
+
+files = list(mc_client.query(query=query,with_metadata=True, with_provenance=True))
+
+
+
+event_count = 0
+fid = 0
+filecount = 0
+
+parents = open("parents.txt",'w')
+for file in files:
+    filecount += 1
+    #print ("a file",file)
+    metadata = file["metadata"]
+    count = metadata["core.event_count"]
+    nfid = len(file["parents"])
+    for p in file["parents"]:
+        parents.write(p['fid'] + "\n")
+    event_count += count
+    fid += nfid
+parents.close()
+print ("pass1 this tag had ",fid,"parents and ",event_count,"events, spread across",filecount,"pass1 files")
+
+dataset = files[0]['metadata']['merge.dataset']
+print ("dataset",dataset)
+inputlist = open("inputfiles.txt",'w')
+query = "files from %s "%dataset
+print (query)
+inputs = mc_client.query(query=query)
+for file in inputs:
+    inputlist.write(file['fid'] + "\n")
+inputlist.close()
+
+if fid != nfiles:
+     print (fid,nfiles)
+     print ("ERROR: final number of files %d is not = the input %d"%(fid,nfiles))
+else:
+     print ("This pass1 is complete",task)
