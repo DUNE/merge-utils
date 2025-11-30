c4d2113bfbdac756dd72e694ad0eb57a5b0cf893
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-11-30 13:09:37 -0800

    batch

--- campaigns/setup_campaign.sh
+++ campaigns/setup_campaign.sh
@@ -1,4 +1,14 @@
+#!/bin/bash 
+
+# script to setup environment variables for a given campaign
+
 export CAMPAIGN=$1
+
+if [ -z "$CAMPAIGN" ]; then
+    echo "Usage: source setup_campaign.sh <campaign_name>"
+    return 1
+fi
+
 export CAMPAIGN_DIR=$MERGE_UTILS_DIR/campaigns/$CAMPAIGN
 export PYTHONPATH=$MERGE_UTILS_DIR/src/prod_utils:$PYTHONPATH
 export CAMPAIGN_CONFIG=$CAMPAIGN_jobs.csv
