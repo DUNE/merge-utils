8f3464c5682367aba21b15ea64700916d442bbd6
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-12-21 08:19:10 -0800

    test root checker

--- src/runners/rootcheck.py
+++ src/runners/rootcheck.py
@@ -5,19 +5,34 @@ import os, sys
 
 
 def rootcheck(names):
+    bad = []
+    gone = []
     for filename in names:
         print ("checking file",filename)
         file = filename.strip()
+
         if not os.path.exists(file):
             print(" does not exist")
+            gone.append(file)
         else:
-            f = TFile.Open(file,'readonly')
-            f.ls()
+            try:
+                f = TFile.Open(file,'readonly')
+                f.ls()
+                f.Close()
+            except:
+                bad.append(file)
+    return bad,gone
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print ("please provide a list of files")
+        print ("rootcheck.py f1 f2 f3 f4")
         sys.exit(1)
     else:
-        rootcheck(sys.argv[1:])
+        bad,gone = rootcheck(sys.argv[1:])
+        for x in bad:
+            print ("BAD",x)
+        for x in gone:
+            print ("MISSING",x)
+            
